5bdbc3bdabfd6e3dcc394b4569073cba08f57c7f
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-05-27 17:39:54 +0200

    generate unique random numbers not working

--- js/script.js
+++ js/script.js
@@ -16,17 +16,32 @@ function generateRandomNumbers(min, max){
     return result;
 }
 
+/* function generateUniqueRandomNumbers(min, max, listaNumeriGiaGenerati){
+    let numeroRandom;
+
+    do{
+        numeroRandom = generateRandomNumbers(min, max);
+    } while (listaNumeriGiaGenerati.incluedes(numeroRandom));
+
+    return numeroRandom;
+} */
+
 function creaCella (xCella, yCella){
     const numeroCelle = xCella * yCella;
     containerGriglia.style.width = `calc(var(--cell-size) * ${xCella})`;
-    
+    //const numeriGenerati =[];
 
     for (let i = 0; i < numeroCelle; i++){
         const numeroRandom = generateRandomNumbers(1, 100);
+        //numeriGenerati.push(numeroRandom);
+
         const cella = document.createElement("div");
+
         cella.classList.add("cella");
-        cella.append(numeroRandom.toString());
-        containerGriglia.append(cella);
+        //cella.append(numeroRandom.toString());
+        cella.innerHTML = `<span>${numeroRandom}</span>`
+
+        containerGriglia.append(cella);        
     }
 }
 
