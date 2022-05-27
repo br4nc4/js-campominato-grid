903b2b0dfbceaec285031be994ce38724ba5a042
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-05-27 17:04:04 +0200

    griglia pt.2 random numbers

--- js/script.js
+++ js/script.js
@@ -1,12 +1,31 @@
 const containerGriglia = document.getElementById("container-griglia");
 
+function generateRandomNumbers(min, max){
+    //controlli sugli argomenti che ricevo
+    if (min === undefined || min === null || min < 0) {
+        // min = 0;
+        return; // vuoto senza niente
+    }
+
+    if (max === undefined || max < min + 1) {
+        max = min + 1;
+        // return; // vuoto senza niente
+    }
+
+    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+    return result;
+}
+
 function creaCella (xCella, yCella){
     const numeroCelle = xCella * yCella;
     containerGriglia.style.width = `calc(var(--cell-size) * ${xCella})`;
+    
 
     for (let i = 0; i < numeroCelle; i++){
+        const numeroRandom = generateRandomNumbers(1, 100);
         const cella = document.createElement("div");
         cella.classList.add("cella");
+        cella.append(numeroRandom.toString());
         containerGriglia.append(cella);
     }
 }
