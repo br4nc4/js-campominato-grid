933c58d5b83cb898ff7ccc50d840e1e62e612aa7
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-05-28 12:58:45 +0200

    numbers inside cells working

--- js/script.js
+++ js/script.js
@@ -1,24 +1,10 @@
 const containerGriglia = document.getElementById("container-griglia");
 
-/* function generateRandomNumbers(min, max){
-    //controlli sugli argomenti che ricevo
-    if (min === undefined || min === null || min < 0) {
-        // min = 0;
-        return; // vuoto senza niente
-    }
-
-    if (max === undefined || max < min + 1) {
-        max = min + 1;
-        // return; // vuoto senza niente
-    }
-
-    const result = Math.floor(Math.random() * (max - min + 1)) + min;
-    return result;
-} */
-
 //funzione che genera un aray di numeri da 1 a 100 
 //in ordine crescente senza doppioni
-function generateOrdinatedNumbers(){ 
+//NON HO TOVATO IL MODO DI AGGIUNGERE I NUMERI ALLE CELLE
+//CON QUESTA FUNZIONE.
+/* function generateOrdinatedNumbers(){ 
     const max = 100; 
     const random = []; 
     for(let i = 0;i<max ; i++){ 
@@ -29,28 +15,20 @@ function generateOrdinatedNumbers(){
         random.sort(function(a, b){return a-b});
     }  
     return random;
-}
+} */
 
 
 function creaCella (xCella, yCella){
     const numeroCelle = xCella * yCella;
     containerGriglia.style.width = `calc(var(--cell-size) * ${xCella})`;
 
-    for (let i = 0; i < numeroCelle; i++){
-        const numeriCella = generateOrdinatedNumbers();
+    for (let i = 1; i < numeroCelle + 1; i++){
         
         const cella = document.createElement("div");
 
         cella.classList.add("cella");
-        //cella.append(numeroRandom.toString());
-        //cella.innerHTML = `<span>${numeri}</span>`
-
-        //cerco di aggiungere ad ogni cella,
-        //ogni singolo numero dell'array
-        //creato nella funzione generateOrdinatedNumbers
-        for (let i = 0; i<numeriCella.length; i++){
-            cella.innerHTML = numeriCella[i];
-        }
+        
+        cella.innerText = `${i}`
 
         containerGriglia.append(cella);        
     }
