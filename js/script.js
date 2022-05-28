c7250b8de23b9126430903518e41237b76a109be
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-05-28 11:45:39 +0200

    function ordinated numbers working. no adding on cells/not working

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const containerGriglia = document.getElementById("container-griglia");
 
-function generateRandomNumbers(min, max){
+/* function generateRandomNumbers(min, max){
     //controlli sugli argomenti che ricevo
     if (min === undefined || min === null || min < 0) {
         // min = 0;
@@ -14,35 +14,49 @@ function generateRandomNumbers(min, max){
 
     const result = Math.floor(Math.random() * (max - min + 1)) + min;
     return result;
+} */
+
+//funzione che genera un aray di numeri da 1 a 100 
+//in ordine crescente senza doppioni
+function generateOrdinatedNumbers(){ 
+    const max = 100; 
+    const random = []; 
+    for(let i = 0;i<max ; i++){ 
+        const calc = Math.floor(Math.random()*max)+1; 
+        if(random.indexOf(calc) == -1){ 
+            random.push(calc); 
+        } else i--;
+        random.sort(function(a, b){return a-b});
+    }  
+    return random;
 }
 
-/* function generateUniqueRandomNumbers(min, max, listaNumeriGiaGenerati){
-    let numeroRandom;
-
-    do{
-        numeroRandom = generateRandomNumbers(min, max);
-    } while (listaNumeriGiaGenerati.incluedes(numeroRandom));
-
-    return numeroRandom;
-} */
 
 function creaCella (xCella, yCella){
     const numeroCelle = xCella * yCella;
     containerGriglia.style.width = `calc(var(--cell-size) * ${xCella})`;
-    //const numeriGenerati =[];
 
     for (let i = 0; i < numeroCelle; i++){
-        const numeroRandom = generateRandomNumbers(1, 100);
-        //numeriGenerati.push(numeroRandom);
-
+        const numeriCella = generateOrdinatedNumbers();
+        
         const cella = document.createElement("div");
 
         cella.classList.add("cella");
         //cella.append(numeroRandom.toString());
-        cella.innerHTML = `<span>${numeroRandom}</span>`
+        //cella.innerHTML = `<span>${numeri}</span>`
+
+        //cerco di aggiungere ad ogni cella,
+        //ogni singolo numero dell'array
+        //creato nella funzione generateOrdinatedNumbers
+        for (let i = 0; i<numeriCella.length; i++){
+            cella.innerHTML = numeriCella[i];
+        }
 
         containerGriglia.append(cella);        
     }
 }
 
 creaCella(10, 10)
+
+
+
